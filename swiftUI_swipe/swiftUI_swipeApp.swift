3f8a672c80a2323fac55829e73b9e94769c88e81
YumiMachino <y.y.1515m@gmail.com>
2021-03-16 09:43:47 -0700

    Initial Commit

---
+++ swiftUI_swipe/swiftUI_swipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_swipeApp.swift
+//  swiftUI_swipe
+//
+//  Created by Yumi Machino on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_swipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
